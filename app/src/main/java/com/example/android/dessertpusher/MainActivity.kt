79ab9cbcc64b84770fafd6b0f95de5a063a0d1d5
Tolea <rezneacanatolie@gmail.com>
2020-04-28 23:31:33 +0300

    Use the timer

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -75,6 +76,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+
+        dessertTimer= DessertTimer(this.lifecycle)
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -152,6 +155,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     }
     override fun onStart() {
         super.onStart()
+//        dessertTimer.startTimer()
         Timber.i("onStart Called")
     }
 
@@ -167,6 +171,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
+//        dessertTimer.stopTimer()
         Timber.i("onStop Called")
     }
 
